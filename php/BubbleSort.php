ecb9644b42bab9ee8874f04a2affa0f4690dfab2
Patrick Sharkey <patrick.sharkey@thehumangeo.com>
2016-10-30 15:46:26 -0400

    Added bubble sort for php

---
+++ php/BubbleSort.php
@@ -0,0 +1,19 @@
+<?php
+
+function bubble_sort($arr) {
+    $size = count($arr);
+    for ($i=0; $i<$size; $i++) {
+        for ($j=0; $j<$size-1-$i; $j++) {
+            if ($arr[$j+1] < $arr[$j]) {
+                swap($arr, $j, $j+1);
+            }
+        }
+    }
+    return $arr;
+}
+
+function swap(&$arr, $a, $b) {
+    $tmp = $arr[$a];
+    $arr[$a] = $arr[$b];
+    $arr[$b] = $tmp;
+}

150f199a932572da93a3e21f3d2f5bbc0ccff2e6
rista404 <risticn6@gmail.com>
2016-10-31 10:10:33 +0100

    Add javascript quicksort

---
+++ javascript/quicksort.js
@@ -0,0 +1,20 @@
+// quicksort :: Ord a => [a] -> [a]
+function quicksort( diff = (a, b) => a - b, array = [] ) {
+	if( array.length === 0 ) return []
+
+	const [x, ...xs] = array
+
+	const lessThan = xs.filter(a => diff(a, x) >= 0)
+	const biggerThan = xs.filter(a => diff(a, x) < 0)
+
+	const lessThanSorted = quicksort(diff, lessThan)
+	const biggerThanSorted = quicksort(diff, biggerThan)
+
+	return [...lessThanSorted, x, ...biggerThanSorted]
+}
+
+export default quicksort
+
+// example usage
+// const diff = (a, b) => a - b
+// console.log(quicksort(diff, [3, 2, 5, 7, 9123, 92, 23, 567, 23]))

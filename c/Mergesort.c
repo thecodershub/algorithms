d7f858f7167a322c688e54c0c88c131f998e9559
İlim Uğur <ilimugur@gmail.com>
2016-10-31 09:49:14 +0200

    Implement Mergesort in C. (#63)
    * Implement Mergesort in C.
    
    * Update to remove duplication in testing function.
    
    * Replace if statements with assertions
    
    * Update README.md to reflect most recent version

---
+++ c/Mergesort.c
@@ -0,0 +1,109 @@
+#include <string.h>
+#include <assert.h>
+#define NLIMIT 100000
+
+void mergeSort(int arr[], int n)
+{
+    int tmp[NLIMIT];
+    int *firstHalf = tmp;
+    int *secondHalf = tmp + n/2;
+    int firstHalfSize = n/2;
+    int secondHalfSize = (n - n/2);
+    int i, p, q;
+
+    if(n < 2)
+        return;
+
+    /*
+     * Copy the two halves of the initial array to a temporary storage.
+     * This temporary location is where the recursive mergeSort call will
+     * work on.
+     */
+    memcpy(firstHalf, arr, n/2 * sizeof(int));
+    memcpy(secondHalf, arr + n/2, (n - n/2) * sizeof(int));
+
+    mergeSort(firstHalf, n/2);
+    mergeSort(secondHalf, n - n/2);
+
+    /* Merge */
+    i = 0;
+    p = 0;
+    q = 0;
+    while(i < n)
+    {
+        if(p < firstHalfSize)
+	{
+            if(q < secondHalfSize)
+            {
+                if(firstHalf[p] < secondHalf[q])
+                {
+                    arr[i++] = firstHalf[p++];
+                }
+                else
+                {
+                    arr[i++] = secondHalf[q++];
+                }
+            }
+            else
+            {
+                arr[i++] = firstHalf[p++];
+            }
+        }
+        else
+        {
+            arr[i++] = secondHalf[q++];
+        }
+    }
+
+    return;
+}
+
+int checkMergeSort(int arr[], int n)
+{
+    int i;
+    mergeSort(arr, n);
+    for(i = 1; i < n; ++i)
+    {
+        if(arr[i-1] > arr[i])
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int testMergeSort()
+{
+    int i, j, arr[NLIMIT];
+    /* Test #1: All 0s */
+
+    for(i = 0; i < NLIMIT; ++i)
+    {
+        arr[i] = 0;
+    }
+    assert(checkMergeSort(arr, NLIMIT) == 0);
+
+    /* Test #2: Range [0, NLIMIT) in increasing order */
+
+    for(i = 0; i < NLIMIT; ++i)
+    {
+        arr[i] = i;
+    }
+    assert(checkMergeSort(arr, NLIMIT) == 0);
+
+    /* Test #3: Range [1, NLIMIT] in decreasing order */
+    j = NLIMIT;
+    for(i = 0; i < NLIMIT; ++i)
+    {
+        arr[i] = j--;
+    }
+    assert(checkMergeSort(arr, NLIMIT) == 0);
+
+    return 0;
+}
+
+int main()
+{
+    testMergeSort();
+    return 0;
+}

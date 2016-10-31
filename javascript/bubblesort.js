e0bc3ffbd2cdfcb19dfb0e79d32f1cbeae6e0b55
Aaron Raff <aaronraffdev@gmail.com>
2016-10-31 09:37:26 -0400

    Added Bubblesort in JS

---
+++ javascript/bubblesort.js
@@ -0,0 +1,17 @@
+/**
+ * Bubble sorts a list of numbers.
+ * @param {Numer|Array} listOfNums - Array of numbers to be sorted.
+ * @return {Numer|Array} listOfNums - Sorted array of initial parameter.
+ */
+function bubblesort(listOfNums) {
+  for(var j = 0; j < listOfNums.length - 1; j++) {
+    for(var i = j + 1; i < listOfNums.length; i++) {
+      if(listOfNums[i] < listOfNums[j]) {
+        var holder = listOfNums[j];
+        listOfNums[j] = listOfNums[i];
+        listOfNums[i] = holder;
+      }
+    }
+  }
+  return listOfNums;
+}

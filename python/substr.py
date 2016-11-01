f8f20774014a3af0f57c7560bee94952dd94de1c
cschristine <cs95200@gmail.com>
2016-10-31 21:47:07 -0700

    Added comments to substring python

--- python/substr.py
+++ python/substr.py
@@ -1,8 +1,16 @@
-#Simply check if a substring is found in the string
+"""
+Simply check if a substring is found in the string
+subst - substring
+st    - string
+"""
 def substr(subst,st):
+    #Get length of substring
     sbLength = len(subst)
+    #Get length of string
     sLength = len(st)
+    #Scroll through string
     for i in range(0, sLength-sbLength+1):
+        #If current window matches with substring
         if st[i:i+sbLength] == subst:
             return true;
             

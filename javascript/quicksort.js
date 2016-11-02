f2456dfbc428459112d41302772cf070fc9effd8
Ben Zhang <benzhangniu@gmail.com>
2016-11-01 23:12:24 -0400

    Add tests

--- javascript/quicksort.js
+++ javascript/quicksort.js
@@ -43,11 +43,115 @@ function quickSort(arr, comp = (a, b) => a < b, lo = 0, hi = arr.length - 1) {
 
 // Usage:
 const arr = [4, 5, 8, 2, 1, 0, 3];
+quickSort(arr); // arr is now [0, 1, 2, 3, 4, 5, 8]
 
-// sort with default comparison
-quickSort(arr);
-console.log(arr);
+// Tests:
+// i => input, o => output, r => reversed output
+const tests = [
+  {
+    i: [],
+    o: [],
+    r: []
+  },
+  {
+    i: [ 0 ],
+    o: [ 0 ],
+    r: [ 0 ]
+  },
+  {
+    i: [ 0, 0 ],
+    o: [ 0, 0 ],
+    r: [ 0, 0 ]
+  },
+  {
+    i: [ 0, 0, 0 ],
+    o: [ 0, 0, 0 ],
+    r: [ 0, 0, 0 ]
+  },
+  {
+    i: [ 0, 1 ],
+    o: [ 0, 1 ],
+    r: [ 1, 0 ]
+  },
+  {
+    i: [ 1, 0 ],
+    o: [ 0, 1 ],
+    r: [ 1, 0 ]
+  },
+  {
+    i: [ 0, 1, 2 ],
+    o: [ 0, 1, 2 ],
+    r: [ 2, 1, 0 ]
+  },
+  {
+    i: [ 0, 2, 1 ],
+    o: [ 0, 1, 2 ],
+    r: [ 2, 1, 0 ]
+  },
+  {
+    i: [ 1, 0, 2 ],
+    o: [ 0, 1, 2 ],
+    r: [ 2, 1, 0 ]
+  },
+  {
+    i: [ 1, 2, 0 ],
+    o: [ 0, 1, 2 ],
+    r: [ 2, 1, 0 ]
+  },
+  {
+    i: [ 2, 0, 1 ],
+    o: [ 0, 1, 2 ],
+    r: [ 2, 1, 0 ]
+  },
+  {
+    i: [ 2, 1, 0 ],
+    o: [ 0, 1, 2 ],
+    r: [ 2, 1, 0 ]
+  },
+  {
+    i: [ 0, 1, 1 ],
+    o: [ 0, 1, 1 ],
+    r: [ 1, 1, 0 ]
+  },
+  {
+    i: [ 1, 0, 1 ],
+    o: [ 0, 1, 1 ],
+    r: [ 1, 1, 0 ]
+  },
+  {
+    i: [ 1, 1, 0 ],
+    o: [ 0, 1, 1 ],
+    r: [ 1, 1, 0 ]
+  },
+  {
+    i: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ],
+    o: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ],
+    r: [ 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 ]
+  },
+  {
+    i: [ 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 ],
+    o: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ],
+    r: [ 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 ]
+  },
+  {
+    i: [ 42, 9, 17, 54, 602, -3, 54, 999, -11 ],
+    o: [ -11, -3, 9, 17, 42, 54, 54, 602, 999 ],
+    r: [ 999, 602, 54, 54, 42, 17, 9, -3, -11 ]
+  },
+  {
+    i: [ -11, -3, 9, 17, 42, 54, 54, 602, 999 ],
+    o: [ -11, -3, 9, 17, 42, 54, 54, 602, 999 ],
+    r: [ 999, 602, 54, 54, 42, 17, 9, -3, -11 ]
+  }
+];
 
-// now reverse it
-quickSort(arr, (a, b) => a > b);
-console.log(arr);
+for (let j = 0; j < tests.length; j++) {
+  let input = tests[j].i.slice(0); // copy the array
+  quickSort(input);
+  console.assert(input.toString() === tests[j].o.toString());
+
+  let input2 = tests[j].i.slice(0); // copy the array
+  quickSort(input2, (a, b) => a > b);
+  console.assert(input2.toString() === tests[j].r.toString());
+}
+

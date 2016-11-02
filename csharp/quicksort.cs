82de1ceb3a0ec2d71ff534541666704be41be2b2
Oleh Huzei <oleg.guz.rus@gmail.com>
2016-11-02 23:50:58 +0200

    Added tests

--- csharp/quicksort.cs
+++ csharp/quicksort.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
  
 namespace Quicksort
 {
@@ -9,51 +7,34 @@ namespace Quicksort
     {
         static void Main(string[] args)
         {
-            // Create an unsorted array of string elements
-            int[] unsorted = { 34, -2, 43, 1, 56, 7, 89, 102, 3};
- 
-            // Print the unsorted array
-            for (int i = 0; i < unsorted.Length; i++)
-            {
-                Console.Write(unsorted[i] + " ");
-            }
- 
-            Console.WriteLine();
- 
-            // Sort the array
-            Quicksort(unsorted, 0, unsorted.Length - 1);
- 
-            // Print the sorted array
-            for (int i = 0; i < unsorted.Length; i++)
-            {
-                Console.Write(unsorted[i] + " ");
-            }
- 
-            Console.WriteLine();
- 
-            Console.ReadLine();
+            TestSortedArray();
+            TestTheWorstCaseArray();
+            TestTwoUniqueNumbersInArray();
+            TestRandomDataInArray();
+            TestOnlyZeroNumbersInArray();
+        }
+
+        public static void QuicksortWraper(int[] elements)
+        {
+            Quicksort(elements, 0, elements.Length - 1);
         }
  
         public static void Quicksort(int[] elements, int left, int right)
         {
-            int i = left, j = right;
+            int i = left;
+            int j = right;
             int pivot = elements[(left + right) / 2];
  
-            while (i <= j)
-            {
-                while (elements[i].CompareTo(pivot) < 0)
-                {
+            while (i <= j) {
+                while (elements[i].CompareTo(pivot) < 0) {
                     i++;
                 }
  
-                while (elements[j].CompareTo(pivot) > 0)
-                {
+                while (elements[j].CompareTo(pivot) > 0) {
                     j--;
                 }
  
-                if (i <= j)
-                {
-                    // Swap
+                if (i <= j) {
                     int tmp = elements[i];
                     elements[i] = elements[j];
                     elements[j] = tmp;
@@ -62,18 +43,73 @@ namespace Quicksort
                     j--;
                 }
             }
- 
-            // Recursive calls
-            if (left < j)
-            {
+            if (left < j) {
                 Quicksort(elements, left, j);
             }
  
-            if (i < right)
-            {
+            if (i < right) {
                 Quicksort(elements, i, right);
             }
         }
- 
+
+        public static void TestSortedArray()
+        {
+            int[] toSort = { 10, 11, 12, 13, 14, 15, 16, 17, 18 };
+            int[] toVerify = { 10, 11, 12, 13, 14, 15, 16, 17, 18 };
+
+            QuicksortWraper(toSort);
+
+            AssertEquals(toSort, toVerify);
+        }
+
+
+        public static void TestTheWorstCaseArray()
+        {
+            int[] toSort = { 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4 };
+            int[] toVerify = { -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            QuicksortWraper(toSort);
+
+            AssertEquals(toSort, toVerify);
+        }
+
+        public static void TestTwoUniqueNumbersInArray()
+        {
+            int[] toSort = { 1, 2, 1, 1, 2, 1, 2, 2 };
+            int[] toVerify = { 1, 1, 1, 1, 2, 2, 2, 2 };
+
+            QuicksortWraper(toSort);
+
+            AssertEquals(toSort, toVerify);
+        }
+
+        public static void TestRandomDataInArray()
+        {
+            int[] toSort = { 800, 11, 50, 771, 649, 770, 240, 9 };
+            int[] toVerify = { 9, 11, 50, 240, 649, 770, 771, 800 };
+
+            QuicksortWraper(toSort);
+
+            AssertEquals(toSort, toVerify);
+        }
+
+        public static void TestOnlyZeroNumbersInArray()
+        {
+            int[] toSort = { 0, 0, 0, 0, 0, 0, 0 };
+            int[] toVerify = { 0, 0, 0, 0, 0, 0, 0 };
+
+            QuicksortWraper(toSort);
+
+            AssertEquals(toSort, toVerify);
+        }
+
+        public static void AssertEquals(int[] a, int[] b)
+        {
+            Debug.Assert(a.Length == b.Length, "Length of arrays are not equals.");
+
+            for (int i = 0; i < a.Length; i++) {
+                Debug.Assert(a[i] == b[i], "Cells of arrays are not equals.");            
+            }
+        }
     }
 }
